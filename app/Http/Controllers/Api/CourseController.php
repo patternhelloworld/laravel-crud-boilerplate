ac66fd2ec2e41d433961757ceac805784a4d9756
Andrew-Kang-G <studypurpose@naver.com>
2023-06-09 00:10:36 +0900

    Remove unnecessary stuff in Models

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -39,7 +39,7 @@ class CourseController extends Controller
 
         try {
 
-            // This is "Soft Delete".
+            // These are all "Soft Delete".
             Course::find($id)->delete();
             Enrollment::where('course_id', $id)->delete();
             Lesson::whereIn('enrollment_id', Enrollment::where('course_id', $id)->get()->pluck('id')->toArray())->delete();
--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -21,7 +21,7 @@ class Enrollment extends Model
      */
     protected $table = 'enrollments';
 
-    protected $fillable = ['students_id', 'course_id', ];
+    protected $fillable = ['students_id', 'course_id'];
 
     /**
      * The attributes that should be mutated to dates.
@@ -39,11 +39,6 @@ class Enrollment extends Model
 
     ];
 
-    protected static function newFactory(): ArticleFactory
-    {
-        return ArticleFactory::new();
-    }
-
     /**
      * Load all for admin and paginate
      *
@@ -55,92 +50,6 @@ class Enrollment extends Model
             ->paginate();
     }
 
-    /**
-     * Load all for logged in user and paginate
-     *
-     * @param $user_id
-     *
-     * @return Paginator
-     */
-    public static function loadAllMine(int $user_id): Paginator
-    {
-        return static::latest()
-            ->mine($user_id)
-            ->paginate();
-    }
-
-    /**
-     * load all published with pagination
-     *
-     * @return Paginator
-     */
-    public static function loadAllPublished(): Paginator
-    {
-        return static::with([
-            'user' => function (BelongsTo $query) {
-                $query->select('id', 'name');
-            },
-        ])
-            ->latest()
-            ->published()
-            ->paginate();
-    }
-
-    /**
-     * load one published
-     *
-     * @param string $slug
-     *
-     * @return Article
-     */
-    public static function loadPublished(string $slug): Enrollment
-    {
-        return static::with([
-            'user' => function (BelongsTo $query) {
-                $query->select('id', 'name');
-            },
-        ])
-            ->published()
-            ->where('slug', $slug)
-            ->firstOrFail();
-    }
-
-    /**
-     * Add query scope to get only published articles
-     *
-     * @param Builder $query
-     *
-     * @return Builder
-     */
-    public function scopePublished(Builder $query): Builder
-    {
-        return $query->where([
-            'published' => true,
-        ]);
-    }
-
-    /**
-     * Load only articles related with the user id
-     *
-     * @param Builder $query
-     * @param int $user_id
-     *
-     * @return Builder
-     */
-    public function scopeMine(Builder $query, int $user_id): Builder
-    {
-        return $query->where('user_id', $user_id);
-    }
-
-    /**
-     * Relationship between articles and user
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
 
     public function student()
     {
